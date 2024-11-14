786afd3d9d25f7bea5255b01ff4ceacb323bdd84
Zettex <safaandsafa4@gmail.com>
2024-11-14 19:15:43 +0600

    two models made

--- building/models.py
+++ building/models.py
@@ -2,10 +2,20 @@ from django.db import models
 from owners.models import Owner
 from tenants.models import Tenant
 
+RENT_CHOICES = [
+    ('Tolet','Tolet'),
+    ('Filled','Filled')
+]
+class Unit(models.Model):
+    name = models.CharField(max_length=255)
+    # rent_status = models.CharField(max_length=255,choices=RENT_CHOICES,default=RENT_CHOICES)
+    bedroom = models.IntegerField()
+    window = models.IntegerField()
+
 class Building(models.Model):
     name = models.CharField(max_length=1000)
     address = models.CharField(max_length=1000)
     owner = models.ManyToManyField(Owner)
-    # tenant = models.OneToOneField(Tenant)
-    # room no
+    unit = models.ManyToManyField(Unit)
+
     
