703067e2a10e6e32333074c50457c0bde2979995
Zettex <safaandsafa4@gmail.com>
2024-11-15 12:33:02 +0600

    modified the models

--- booking/models.py
+++ booking/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from building.models import Unit
 from tenants.models import Tenant
+from django.core.validators import MinValueValidator
 
 class Booking(models.Model):
     unit = models.OneToOneField(Unit,on_delete=models.CASCADE)
-    rent = models.IntegerField()
+    rent = models.IntegerField(validators=[MinValueValidator(0)])
     is_booked = models.BooleanField(default=True)
-    tenant = models.ForeignKey(Tenant,on_delete=models.SET_NULL,blank=True)
+    tenant = models.ForeignKey(Tenant,on_delete=models.SET_NULL,blank=True,null=True)
     created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.unit.name
--- building/models.py
+++ building/models.py
@@ -1,22 +1,18 @@
 from django.db import models
 from owners.models import Owner
 from tenants.models import Tenant
-
-RENT_CHOICES = [
-    ('To-let','To-let'),
-    ('Filled','Filled')
-]
+from django.core.validators import MinValueValidator
 
 class Building(models.Model):
-    name = models.CharField(max_length=1000)
-    address = models.CharField(max_length=1000)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
     owner = models.ManyToManyField(Owner)
     image = models.ImageField(upload_to='building/images/building_image')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
-        return self.name
+        return f'{self.name} - {self.address[:30]}'
     
     class Meta:
         ordering = ['-created']
@@ -26,18 +22,18 @@ class Building(models.Model):
     
 class Unit(models.Model):
     name = models.CharField(max_length=255)
-    room = models.IntegerField()
-    toilet = models.IntegerField()
-    window = models.IntegerField()
-    building = models.ForeignKey(Building,on_delete=models.CASCADE,related_name='unit')
+    room = models.IntegerField(validators=[MinValueValidator(1)])
+    toilet = models.IntegerField(validators=[MinValueValidator(1)])
+    window = models.IntegerField(validators=[MinValueValidator(0)])
+    building = models.ForeignKey(Building,on_delete=models.CASCADE,related_name='units')
     image = models.ImageField(upload_to='building/images/unit_image')
-    tenant = models.ForeignKey(Tenant,on_delete=models.CASCADE,blank=True,null=True,related_name='unit')
+    tenant = models.ForeignKey(Tenant,on_delete=models.CASCADE,blank=True,null=True,related_name='units')
     is_occupied = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
-        return self.name
+        return f'{self.name}-{self.building.name}'
     
     class Meta:
         ordering = ['updated']
