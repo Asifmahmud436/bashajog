65a86a92e45551d5ad8a4da6ee7f1848dbb88825
Zettex <safaandsafa4@gmail.com>
2024-11-15 19:26:06 +0600

    booking api done

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
+from .models import Booking
+from .serializers import BookingSerializer
 
-# Create your views here.
+
+class BookingPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 500
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    pagination_class = BookingPagination
+
--- django_project/urls.py
+++ django_project/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
     path('building/',include('building.urls')),
+    path('booking/',include('booking.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
