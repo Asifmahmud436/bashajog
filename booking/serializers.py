992f7a01b33284d57ba11b34904b00a18bbbe1c9
Zettex <safaandsafa4@gmail.com>
2024-11-17 02:59:36 +0600

    booking query completed

--- booking/serializers.py
+++ booking/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import Booking
 
 class BookingSerializer(serializers.ModelSerializer):
+    unit_name = serializers.CharField(source='unit.name',read_only=True)
+    building_name = serializers.CharField(source='unit.building.name',read_only=True)
+    tenant_name = serializers.CharField(source='tenant.user.username',read_only=True)
+    
     class Meta:
         model = Booking
         fields = '__all__'
--- booking/views.py
+++ booking/views.py
@@ -3,6 +3,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 from .models import Booking
 from .serializers import BookingSerializer
+from django.db.models import Q
 
 
 class BookingPagination(PageNumberPagination):
@@ -13,4 +14,11 @@ class BookingViewSet(ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     pagination_class = BookingPagination
+
+    def get_queryset(self):
+        queryset = Booking.objects.all()
+        owner_username = self.request.query_params.get('owner_username')
+        if owner_username:
+            queryset = queryset.filter(unit__building__owner__user__username__icontains=owner_username).distinct()
+        return queryset
 
