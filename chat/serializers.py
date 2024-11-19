9d38f36a7b02b6298e94629752c398e203970b63
Zettex <safaandsafa4@gmail.com>
2024-11-19 19:58:01 +0600

    made the serializer of chat app

---
+++ chat/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import ChatRoom,Message
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender_name = serializers.CharField(source='sender.username',read_only=True)
+    sender_type = serializers.CharField(source='sender.username',read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ['id', 'content', 'sender_name','sender_type','timestamp']
+
+class ChatRoomSerializer(serializers.ModelSerializer):
+    messages = MessageSerializer(many=True,read_only=True)
+    building_name = serializers.CharField(source='building.name',read_only=True)
+
+    class Meta:
+        model = ChatRoom
+        fields = ['id','building_name','name','messages','websocket_url']
+
+    def get_websocket_url(self,obj):
+        return f'ws://{self.context["request"].get_host()}/ws/chat/{obj.id}/'
